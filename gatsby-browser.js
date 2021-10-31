8d0202f1db3030237a2f5e0d05b9089655a0a9e6
Isiah Fletcher <Isiahfletcher7@mail.com>
2021-10-31 17:47:41 -0600

    Switch to default export

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Layout from './src/components/Layout';
+// Fonts
+import '@fontsource/roboto-mono/variable.css';
 
-export const wrapPageElement = ({ element, props }) => (
+const wrapPageElement = ({ element, props }) => (
   <Layout {...props}>{element}</Layout>
 );
 
-// Fonts
-import '@fontsource/roboto-mono/variable.css';
+export default wrapPageElement;
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Layout from './src/components/Layout';
 
-export const wrapPageElement = ({ element, props }) => (
+const wrapPageElement = ({ element, props }) => (
   <Layout {...props}>{element}</Layout>
 );
+
+export default wrapPageElement;
