816316fd3eaf97b1a86a04e24bde0f047e382e01
Isiah Fletcher <Isiahfletcher7@mail.com>
2021-11-01 08:57:10 -0600

    remove arrow function and add back gatsby-ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Layout from './src/components/Layout';
-// Fonts
-import '@fontsource/roboto-mono/variable.css';
 
 export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>;
