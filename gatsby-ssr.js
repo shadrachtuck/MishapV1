1d2c46b1b0f79caf32d8aaa33f8698583f625858
Isiah Fletcher <Isiahfletcher7@mail.com>
2021-11-01 08:42:57 -0600

    switch to es5 imports

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import Layout from './src/components/Layout';
+const React = require('react');
+const Layout = require('./src/components/Layout');
 
-export const wrapPageElement = ({ element, props }) => (
+exports.wrapPageElement = ({ element, props }) => (
   <Layout {...props}>{element}</Layout>
 );
