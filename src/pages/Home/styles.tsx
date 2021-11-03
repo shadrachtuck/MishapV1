09718fbdc706b572fe6fe6b95ad0d34567b2e7a6
Isiah Fletcher <Isiahfletcher7@mail.com>
2021-11-03 08:34:15 -0600

    remove any pages without default export and reorganize home page

--- src/pages/Home/styles.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const PageSection = styled.section`
-  display: flex;
-  flex-flow: column nowrap;
-  align-items: center;
-  justify-content: center;
-`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,55 @@
+import ArtistCards from '../components/ArtistCards';
+import { graphql, useStaticQuery } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 import React from 'react';
+import { Strapi } from '../typings/strapi';
+import styled from 'styled-components';
 import Layout from '../components/Layout';
-import Home from './Home';
 
-export default (): JSX.Element => (
-  <Layout>
-    <Home />
-  </Layout>
-);
+export const query = graphql`
+  query GET_ARTISTS {
+    strapi {
+      artists {
+        slug
+        name
+        bio
+        socialMedia: social_media {
+          ...StrapiArtistSocialMedia
+        }
+        profilePicture: profile_picture {
+          ...StrapiUploadFile
+        }
+      }
+    }
+  }
+`;
+
+export const PageSection = styled.section`
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  justify-content: center;
+`;
+
+const BUNNY = '../assets/svg/rabbit_black.svg';
+
+const Home = (): JSX.Element => {
+  const {
+    strapi: { artists },
+  } = useStaticQuery<Strapi>(query);
+
+  return (
+    <Layout>
+      <PageSection id="home">
+        <StaticImage src={BUNNY} alt="" />
+        <h1>Mishap Records</h1>
+      </PageSection>
+
+      <PageSection id="artists">
+        <ArtistCards artists={artists} />
+      </PageSection>
+    </Layout>
+  );
+};
+
+export default Home;
