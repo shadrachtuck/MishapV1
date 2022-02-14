99a65f70e03eef3810784a38ffac74805594d595
Isiah Fletcher <Isiahfletcher7@mail.com>
2022-02-14 10:01:03 -0700

    Switch to useEffectfor SSR

--- src/utils/hooks/useIsAboveMobileWidth.ts
+++ src/utils/hooks/useIsAboveMobileWidth.ts
@@ -1,4 +1,4 @@
-import { useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { theme } from '../../styled-components/utils/defaultTheme';
 import removeRemFromString from '../functions/removeRemFromString';
 
@@ -8,7 +8,7 @@ const useIsAboveMobileWidth = (): boolean | undefined => {
   const [isAboveMobile, setIsAboveMobile] = useState(false);
   const mobileBreakpoint = removeRemFromString(theme.breakpoints.medium) * 16;
 
-  useMemo(() => {
+  useEffect(() => {
     if (!isSSR) {
       if (typeof window !== undefined && window.innerWidth > mobileBreakpoint)
         setIsAboveMobile(true);
