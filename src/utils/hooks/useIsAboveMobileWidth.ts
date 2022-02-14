431ab6e3abca44f5f21e926cc324684d79ae27c8
Isiah Fletcher <Isiahfletcher7@mail.com>
2022-02-14 08:55:03 -0700

    Add undefined check for SSR

--- src/utils/hooks/useIsAboveMobileWidth.ts
+++ src/utils/hooks/useIsAboveMobileWidth.ts
@@ -2,16 +2,19 @@ import { useMemo, useState } from 'react';
 import { theme } from '../../styled-components/utils/defaultTheme';
 import removeRemFromString from '../functions/removeRemFromString';
 
+const isSSR = typeof window === undefined;
+
 const useIsAboveMobileWidth = (): boolean | undefined => {
   const [isAboveMobile, setIsAboveMobile] = useState(false);
   const mobileBreakpoint = removeRemFromString(theme.breakpoints.medium) * 16;
 
   useMemo(() => {
+    if (isSSR) return;
     if (window.innerWidth > mobileBreakpoint) setIsAboveMobile(true);
     else setIsAboveMobile(false);
   }, [mobileBreakpoint]);
 
-  if (typeof window === undefined || window.innerWidth === undefined) return;
+  if (isSSR || window.innerWidth === undefined) return;
   if (!mobileBreakpoint) return;
   return isAboveMobile;
 };
