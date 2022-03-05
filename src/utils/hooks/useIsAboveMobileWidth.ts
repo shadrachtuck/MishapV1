7fbbf50c1cbabfae07d6ed6c6e34fc67ab82c106
Isiah Fletcher <Isiahfletcher7@mail.com>
2022-03-05 11:25:43 -0700

    Add ssr check

--- src/utils/hooks/useIsAboveMobileWidth.ts
+++ src/utils/hooks/useIsAboveMobileWidth.ts
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import { theme } from '../../styled-components/utils/defaultTheme';
 import removeRemFromString from '../functions/removeRemFromString';
 
+const isSSR = window === undefined;
+
 const useIsAboveMobileWidth = (): boolean | undefined => {
   const [isAboveMobile, setIsAboveMobile] = useState<boolean | undefined>(
     undefined,
@@ -9,11 +11,12 @@ const useIsAboveMobileWidth = (): boolean | undefined => {
   const [width, setWidth] = useState(window.innerWidth);
   const mobileBreakpoint = removeRemFromString(theme.breakpoints.medium) * 16;
 
-  useEffect(() => console.log(width), [width]);
   useEffect(() => {
-    const handleWindowResize = (): void => setWidth(window.innerWidth);
-    window.addEventListener('resize', handleWindowResize);
-    return () => window.removeEventListener('resize', handleWindowResize);
+    if (!isSSR) {
+      const handleWindowResize = (): void => setWidth(window.innerWidth);
+      window.addEventListener('resize', handleWindowResize);
+      return () => window.removeEventListener('resize', handleWindowResize);
+    }
   }, []);
 
   useEffect(() => {
